3a035c81093e3044ec3413e5f6fae0fa86839fec
Eric Einerson <eaeiner.gmail.co>
2022-01-02 15:57:49 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+var el = document.getElementById('main')
+el.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id','victory')
+newHeader.textContent = "YOUR-NAME is the champion"
